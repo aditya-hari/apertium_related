8c6fa4eefad67449018372e8276f401f847b6d00
Aditya <adityahari3791@gmail.com>
2020-03-03 15:34:25 +0530

    First

---
+++ classify_symbols.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<unicode/uchar.h>
+#include<unicode/ustdio.h>
+
+int main(int argc, char** argv){
+	UChar32 c;
+    UFILE *in = u_finit(stdin, NULL, NULL);
+    UFILE *out = u_get_stdout();
+    const char *name;
+    int val;
+    while((c = u_fgetcx(in))!=U_EOF){
+        val = u_charType(c);
+        name = u_getPropertyValueName(UCHAR_GENERAL_CATEGORY, val, U_SHORT_PROPERTY_NAME);
+        u_fputc(c, out);
+        std::cout << " " << name <<"\n";
+    }
+}
